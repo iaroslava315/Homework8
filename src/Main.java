e1cafba085419cc8f138e0159f566af7288305c0
iaroslava315 <iaroslava.315@gmail.com>
2026-04-27 17:33:21 +0700

    Исправлено ДЗ

--- src/Main.java
+++ src/Main.java
@@ -11,6 +11,8 @@ void main() {
 
         double[] weights = {1.57, 7.654, 9.986};
 
+        int bigBox[] = {1, 6, 15, 28, 69};
+
 
     }
 
@@ -22,10 +24,32 @@ void main() {
         arr[0] = 1;
         arr[1] = 2;
         arr[2] = 3;
-        System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            if (i == arr.length - 1) {
+                System.out.println(arr[i]);
+                break;
+            }
+            System.out.print(arr[i] + ", ");
+        }
 
         double[] weights = {1.57, 7.654, 9.986};
-        System.out.println(Arrays.toString(weights));
+        for (int j = 0; j < weights.length; j++) {
+            if (j == weights.length - 1) {
+                System.out.println(weights[j]);
+                break;
+            }
+            System.out.print(weights[j] + ", ");
+        }
+
+        int bigBox[] = {1, 6, 15, 28, 69};
+        for (int k = 0; k < bigBox.length; k++) {
+            if (k == bigBox.length - 1) {
+                System.out.println(bigBox[k]);
+                break;
+            }
+            System.out.print(bigBox[k] + ", ");
+        }
+
     }
 
 
@@ -53,16 +77,25 @@ void main() {
             }
             System.out.print(weights[j] + ", ");
         }
+
+        int bigBox[] = {1, 6, 15, 28, 69};
+        for (int k = bigBox.length - 1; k >= 0; k--) {
+            if (k == 0) {
+                System.out.println(bigBox[k]);
+                break;
+            }
+            System.out.print(bigBox[k] + ", ");
+        }
     }
 
 
 //  Task 4
     {
         System.out.println("Task 4");
-        int [] arr = {1,2,3,4,5};
-        for (int i = 0; i < arr.length; i++ ) {
+        int[] arr = {1, 2, 3, 4, 5};
+        for (int i = 0; i < arr.length; i++) {
             if (arr[i] % 2 != 0) {
-                arr [i] += 1;
+                arr[i] += 1;
             }
         }
         System.out.println(Arrays.toString(arr));
